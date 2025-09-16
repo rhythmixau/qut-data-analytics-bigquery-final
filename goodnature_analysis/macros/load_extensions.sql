dd6e233d081dff0352ebe538afef91a67fe6c618
Jack Toke <jack.toke@rhythmix.com.au>
2025-09-16 22:58:52 +1000

    Switched to BigQuery due to problems getting the distance to function properly for MotherDuck

---
+++ goodnature_analysis/macros/load_extensions.sql
@@ -0,0 +1,4 @@
+{% macro load_spatial_extensions() %}
+    INSTALL spatial;
+    LOAD spatial;
+{% endmacro %}
---
+++ goodnature_analysis/models/staging/stg_au_suburbs.sql
@@ -0,0 +1,21 @@
+WITH suburbs AS (
+    SELECT * FROM {{ ref("australian_suburbs") }}
+)
+SELECT
+    ssc_code AS suburb_id,
+    suburb,
+    urban_area,
+    postcode,
+    state,
+    state_name,
+    type AS suburb_type,
+    local_goverment_area,
+    statistic_area,
+    elevation,
+    population,
+    median_income,
+    sqkm,
+    lat AS latitude,
+    lng AS longitude,
+    timezone AS suburb_timezone
+FROM suburbs
